efbbd899187e144b52df0656456743c6f74c1e90
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-07-09 12:49:11 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,21 +1,88 @@
-int check_ch(const char *ch)
+
+int add_arr(int *arr,int cnt)
+{
+    int i = 0;
+    int total = 0;
+    while(i < cnt)
+    {
+        total += arr[i];
+        i++;
+    }
+
+    return total;
+}
+int check_ch(const char ch)
+{
+    if(ch >= '0' && ch <= '9')
+    {
+        return 1;
+    }
+    return 0;
+}
+int sum(const char *ch)
+{
+    int str_len = strlen(ch);
+    int i =0,j=0,num = 0;
+    int total = 0;
+    int num_arr[100] = {0};
+    while(ch[i] != '\0')
+    {
+        if(check_ch(ch[i]))
+        {
+            num = num * 10 + ch[i] - '0';
+            if(check_ch(ch[i+1]))
+            {
+                i++;
+            }
+            else
+            {
+                if(num < 1000)
+                {
+                  num_arr[i] = num;
+                  num = 0;
+                  i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+        else
+        {
+            i++;
+        }
+    }
+
+    total = add_arr(num_arr,i);
+
+    return total;
+}
+
+int check_string(const char *ch)
 {
     int i = 0;
     int str_len = strlen(ch);
-    
+
     if(ch != NULL && str_len > 0)
     {
         return 1;
     }
-    
+
     return 0;
 }
 int add(const char *ch)
 {
-    if(check_ch(ch))
+    if(check_string(ch))
     {
+        int total = sum(ch);
+
+        if(total > 0)
+        {
+            return total;
+        }
         return 0;
     }
-   
+
     return -1;
 }
